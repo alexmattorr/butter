7323f6653ebecfd62f0d565e23561352051ed1f1
alexsmander <alexmattorr@gmail.com>
2014-11-22 11:06:25 -0800

    Made image slider into self contained function.

--- js/script.js
+++ js/script.js
@@ -25,39 +25,41 @@ function signinOverlay() {
 };
 
 // Image Slider
-$(document).ready(function() {
-	$(".carousel-next").on("click", function(e) {
-		var currentActiveImage = $(".image-active");
-		var nextActiveImage = currentActiveImage.next();
+function imgSlider() {
+	$(document).ready(function() {
+		$(".carousel-next").on("click", function(e) {
+			var currentActiveImage = $(".image-active");
+			var nextActiveImage = currentActiveImage.next();
 
-		if(nextActiveImage.length == 0) {
-			nextActiveImage = $(".carousel-inner img").first();
-		};
+			if(nextActiveImage.length == 0) {
+				nextActiveImage = $(".carousel-inner img").first();
+			};
 
-		currentActiveImage.removeClass("image-active").addClass("image-hidden");
-		nextActiveImage.addClass("image-active").removeClass("image-hidden");
-		$(".carousel-inner img").not([currentActiveImage, nextActiveImage]);
+			currentActiveImage.removeClass("image-active").addClass("image-hidden");
+			nextActiveImage.addClass("image-active").removeClass("image-hidden");
+			$(".carousel-inner img").not([currentActiveImage, nextActiveImage]);
 
-		e.preventDefault();
+			e.preventDefault();
+		});
+
+		$(".carousel-previous").on("click", function(e) {
+			var currentActiveImage = $(".image-active");
+			var nextActiveImage = currentActiveImage.prev();
+
+			if(nextActiveImage.length == 0) {
+				nextActiveImage = $(".carousel-inner img").last();
+			}
+
+			currentActiveImage.removeClass("image-active").addClass("image-hidden");
+			nextActiveImage.addClass("image-active").removeClass("image-hidden");
+			$(".carousel-inner img").not([currentActiveImage, nextActiveImage]);
+
+			e.preventDefault();
+		});
 	});
-
-	$(".carousel-previous").on("click", function(e) {
-		var currentActiveImage = $(".image-active");
-		var nextActiveImage = currentActiveImage.prev();
-
-		if(nextActiveImage.length == 0) {
-			nextActiveImage = $(".carousel-inner img").last();
-		}
-
-		currentActiveImage.removeClass("image-active").addClass("image-hidden");
-		nextActiveImage.addClass("image-active").removeClass("image-hidden");
-		$(".carousel-inner img").not([currentActiveImage, nextActiveImage]);
-
-		e.preventDefault();
-	});
-});
-
+};
 
 mobileToggle();
 mobileColor();
 signinOverlay();
+imgSlider();
