3da337b57649973dc78b0f3471c76c2b9f77afa4
alexsmander <alexmattorr@gmail.com>
2014-11-30 15:36:11 -0800

    Login

--- php/login.php
+++ php/login.php
@@ -1,3 +1,30 @@
 <?php
+	$username = $_POST['username'];
+	$password = $_POST['password'];
 
+	if ($username && $password) {
+		$connect = mysql_connect("localhost", "root", "root") or die("Couldn't connect!");
+		mysql_select_db(phplogin) or die("Couldn't find db");
+
+		$query  = mysql_query("SELECT * FROM users WHERE username='$username'");
+		$numrows = mysql_num_rows($query);
+
+		if ($numrows != 0) {
+			while ($row = mysql_fetch_assoc($query)) {
+				$dbusername = $row['username'];
+				$dbpassword = $row['password'];
+			}
+
+			if ($username == $dbusername && $password == $dbpassword) {
+				echo "Your'e In!";
+			} else {
+				echo "Incorrect password!";
+			}
+		} else {
+			die("That user doesn't exist!");
+		}
+
+	} else {
+		die("Please enter a username and a password");
+	}
 ?>
