daacf5a8224f55f3591210b9a2b93e439843986d
alexsmander <alexmattorr@gmail.com>
2014-11-30 17:27:29 -0800

    Reverted to old.

--- php/database/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$connect_error = 'Sorry, we are experiencing connection issues.';
-	mysql_connect('localhost', 'root', 'root') or die($connect_error);
-	mysql_select_db('phplogin') or die($connect_error);
-?>
